1d7b04191a243b8373520b31840d2f6b27773dd9
oc2018 <ontime.c23@gmail.com>
2023-07-05 23:08:45 +0300

    modified:   client/src/userContext.js 	modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ import postRoutes from  './routes/postRoutes.js';
 
 const app = express();
 dotenv.config();
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-// app.use(cors({ credentials: true, origin: 'https://myblog-iz43.vercel.app' }));
+// app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors({ credentials: true, origin: 'https://myblog-iz43.vercel.app' }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static( 'uploads'));
